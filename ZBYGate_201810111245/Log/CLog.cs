7d0f0a372bf6f9896c4fad6e6e99a3ef92fb070e
lench <lench870888@hotmail.com>
2018-10-13 16:35:30 +0800

    Cyliang

---
+++ ZBYGate_201810111245/Log/CLog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZBYGate_201810111245.Log
+{
+    class CLog
+    {
+    }
+}
